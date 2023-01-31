fbbf06386edd2a565ed6d7af0d46f162280be3d7
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-01-31 11:24:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("a")
     basic.clearScreen()
+    playerA += 1
+    rounds += 1
+    justShowTheSCOREBOARD()
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("t")
     basic.clearScreen()
+    tie += 1
+    rounds += 1
+    justShowTheSCOREBOARD()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("b")
     basic.clearScreen()
+    playerB += 1
+    rounds += 1
+    justShowTheSCOREBOARD()
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    reset()
 })
 function justShowTheSCOREBOARD () {
     OLED.clear()
@@ -22,6 +31,16 @@ function justShowTheSCOREBOARD () {
     OLED.writeStringNewLine("Ties:" + tie)
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + rounds)
+}
+function reset () {
+    OLED.init(128, 64)
+    playerA = 0
+    playerB = 0
+    rounds = 0
+    tie = 0
+    OLED.writeStringNewLine("Let's play a game... have you heard of rock, paper, scissors?")
+    basic.pause(3000)
+    justShowTheSCOREBOARD()
 }
 let rounds = 0
 let tie = 0
